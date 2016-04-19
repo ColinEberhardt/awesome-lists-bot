fb832aff9bf067a9ba7ad71b9e40ed44aebbbf36
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-04-19 17:45:45 +0100

    A bit of a refactor

---
+++ README.md
@@ -0,0 +1,5 @@
+# Awesome Stars Bot
+
+I think the GitHub 'awesome' lists are really awesome! However, maintaining them is a struggle - as they grow in size, quality tends to be an issue as links become broken, repos become renamed. Furthermore, as lists grow in size, it becomes harder to find the more significant projects.
+
+This bot (which is very much in beta) is designed to help improve the quality of awesome lists by link checking, following redirects, adding star counts and more ...
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ProgressBar = require('progress');
 const awesomeStars = require('awesome-stars');
+const awesomeLinks = require('awesome-link-checker');
 const program = require('commander');
 const fs = require('fs');
 const path = require('path');
@@ -21,8 +22,8 @@ if (!program.username || !program.password) {
   process.exit();
 }
 
-const sideEffect = side => d => {
-  side(d)
+const sideEffect = fn => d => {
+  fn(d)
   return d;
 };
 
@@ -34,6 +35,11 @@ const rejectIfTrue = (fn, msg) => d => {
   }
 }
 
+const merge = (promise, projection) => d =>
+  promise(d)
+    .then(projection)
+    .then(result => Object.assign({}, d, result));
+
 const github = new GitHubApi({
   version: '3.0.0'
 });
@@ -42,53 +48,37 @@ github.authenticate({
     token: program.password
 });
 
-const updateRepoReadme = (repoName) => {
+const reportRateLimit = () => {
+  return Q.nfcall(github.misc.rateLimit, {})
+    .then(d => d.resources.core.remaining)
+    .then(sideEffect(d => console.log('Rate limit remaining', d)));
+}
 
-  const getReadmeForRepo = () => {
-    return Q.nfcall(github.repos.getContent, {
-        user: program.username,
-        repo: repoName,
-        path: 'README.md'
-      })
-      .then(sideEffect(d => console.log('Fetched README for repo ' + repoName)))
-      .then(res => {
-        return {
-          content: new Buffer(res.content, 'base64').toString('utf8'),
-          sha: res.sha
-        };
-      });
+const getRepoOwner = repoData =>
+  Q.nfcall(github.repos.get, {
+      user: program.username,
+      repo: repoData.repoName,
+    })
+    .then(repoData => repoData.parent.owner.login);
+
+const getReadmeForRepo = repoData => {
+  return Q.nfcall(github.repos.getContent, {
+      user: program.username,
+      repo: repoData.repoName,
+      path: 'README.md'
+    })
+    .then(sideEffect(d => console.log('Fetched README for repo ' + repoData.repoName)))
+    .then(res => {
+      return {
+        content: new Buffer(res.content, 'base64').toString('utf8'),
+        sha: res.sha
+      };
+    });
   };
 
-  const writeReadmeToRepo = (updatedMarkdown) =>
-    Q.nfcall(github.repos.updateFile, {
-        user: program.username,
-        repo: repoName,
-        path: 'README.md',
-        message: 'Updated stars and redirects via awesome-stars-bot',
-        sha: updatedMarkdown.sha,
-        content: new Buffer(updatedMarkdown.updatedContent).toString('base64')
-      })
-      .then(sideEffect(d => console.log('Written README for repo ' + repoName)));
-
-  const createPullRequest = () =>
-    Q.nfcall(github.repos.get, {
-        user: program.username,
-        repo: repoName,
-      })
-      .then(repoData => repoData.parent.owner.login)
-      .then(sideEffect(d => console.log('Creating PR against upstream repo ' + d)))
-      .then(upstreamUser => Q.nfcall(github.pullRequests.create, {
-        user: upstreamUser,
-        repo: repoName,
-        title: 'Updated stars and redirects via awesome-stars-bot',
-        body: prBody,
-        base: 'master',
-        head: program.username + ':master'
-      }));
-
-  const addAwesomeStars = (markdown) => {
+const addAwesomeStars = (repoData) => {
     var bar;
-    return awesomeStars(markdown.content, program.username, program.password,
+    return awesomeStars(repoData.content, program.username, program.password,
       (count) => {
         if (count) {
           bar = new ProgressBar('Fetching stars: [:bar] :percent', { total: count, width: 30 });
@@ -96,29 +86,87 @@ const updateRepoReadme = (repoName) => {
           bar.tick();
         }
       })
-      .then((updatedMarkdown) => Object.assign(markdown, { updatedContent: updatedMarkdown }));
   };
 
-  return getReadmeForRepo()
-    .then(addAwesomeStars)
-    .then(sideEffect(d => console.log('checking for differences')))
-    .then(rejectIfTrue(d => d.content === d.updatedContent, 'markdown has not changed'))
-    .then(markdown => writeReadmeToRepo(markdown));
-    .then(() => createPullRequest())
-};
+const checkLinks = (repoData) => {
+    var bar;
+    return awesomeLinks(repoData.content,
+      (count) => {
+        if (count) {
+          bar = new ProgressBar('Checking links: [:bar] :percent', { total: count, width: 30 });
+        } else {
+          bar.tick();
+        }
+      })
+  };
 
-const reportRateLimit = () => {
-  return Q.nfcall(github.misc.rateLimit, {})
-    .then(d => d.resources.core.remaining)
-    .then(sideEffect(d => console.log('Rate limit remaining', d)));
-}
+const writeReadmeToRepo = (repoData) =>
+  Q.nfcall(github.repos.updateFile, {
+      user: program.username,
+      repo: repoData.repoName,
+      path: 'README.md',
+      message: 'Updated stars and redirects via awesome-stars-bot',
+      sha: repoData.sha,
+      content: new Buffer(repoData.content).toString('base64')
+    });
 
+const createPullRequest = (repoData) =>
+  Q.nfcall(github.pullRequests.create, {
+    user: repoData.repoOwner,
+    repo: repoData.repoName,
+    title: 'Updated stars and redirects via awesome-stars-bot',
+    body: prBody,
+    base: 'master',
+    head: program.username + ':master'
+  });
+
+const getUpstreamPullRequests = (repoData) =>
+  Q.nfcall(github.pullRequests.getAll, {
+    user: repoData.repoOwner,
+    repo: repoData.repoName
+  });
+
+const updateToUpstream = (repoData) =>
+  Q.nfcall(github.gitdata.getReference, {
+    user: repoData.repoOwner,
+    repo: repoData.repoName,
+    ref: 'heads/master'
+  })
+  .then(refData => {
+    return Q.nfcall(github.gitdata.updateReference, {
+      user: 'awesome-stars-bot',
+      repo: repoData.repoName,
+      ref: 'heads/master',
+      sha: refData.object.sha,
+      force: true
+    })
+  });
 
 Q.nfcall(github.repos.getAll, {})
+  // fetch all the repos that this bot operates on and select one to update
   .then(sideEffect(d => console.log('Fetched ' + d.length + ' repos')))
-  .then(repos => _.sortBy(repos, repo => Date.parse(repo.updated_at)))
-  .then(repos => _.last(repos).name)
-  .then(sideEffect(d => console.log('Updating ' + d)))
-  .then(updateRepoReadme)
+  .then(repos => _.sortBy(repos, repo => -Date.parse(repo.updated_at)))
+  .then(repos => { return { repoName: _.last(repos).name }; })
+  .then(sideEffect(d => console.log('Updating ' + d.repoName)))
+  // get the owner, for the purposes of PRs etc ...
+  .then(merge(getRepoOwner, d => { return {repoOwner: d}; }))
+  // check if the bot already has a pending PR
+  .then(merge(getUpstreamPullRequests, d => { return {upstreamPRs: d}; }))
+  .then(rejectIfTrue(d => d.upstreamPRs.some(pr => pr.user.login === 'awesome-stars-bot'), 'There is already a PR pending - Aborting!'))
+  // update the bot's fork
+  .then(merge(updateToUpstream, d => d))
+  // get the README and update
+  .then(merge(getReadmeForRepo, d => { return {content: d.content, original: d.content, sha: d.sha}; }))
+  .then(merge(addAwesomeStars, d => { return {content: d}; }))
+  .then(merge(checkLinks, d => { return {content: d}; }))
+  // check if this has resulted in changes
+  .then(sideEffect(d => console.log('Checking for differences')))
+  .then(rejectIfTrue(d => d.original === d.content, 'Markdown has not changed - Aborting!'))
+  // write the changes
+  .then(merge(writeReadmeToRepo, d => d))
+  .then(sideEffect(d => console.log('Written README for repo ' + d.repoName)))
+  // create the PR
+  .then(merge(createPullRequest, d => d))
+  .then(sideEffect(d => console.log('PR submitted - all done :-)')))
   .catch(console.error)
-  .finally(reportRateLimit)
+  .finally(reportRateLimit);
